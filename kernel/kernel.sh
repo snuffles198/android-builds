1d4831a41672e7c928e51c84b8de92206a693720
Joe7500 <j7545032@gmail.com>
2025-05-13 21:41:02 +0200

    add kernel

---
+++ kernel/kernel.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -v
+
+base_root=`pwd`
+
+# Setup
+#rm -rf `ls -d * | grep -v tar.xz | grep -v tar.gz`
+#rm -rf `ls * | grep -v tar.xz | grep -v tar.gz`
+
+wget https://github.com/Joe7500/Builds/releases/download/Stuff/kernel.tar.xz || exit 1
+wget https://github.com/Joe7500/Builds/releases/download/Stuff/toolchain.tar.xz || exit 1
+
+mkdir work
+cd $base_root/work
+
+tar xf ../kernel.tar.xz
+cd kernel/xiaomi/chime && tar czf ../../../../kernel.tar.gz . && cd ../../..
+if [ $? -ne 0 ]; then exit 1 ; fi
+rm -rf kernel/xiaomi/chime
+
+tar xf ../toolchain.tar.xz
+mv prebuilts/clang/host/linux-x86/clang-stablekern/ toolchain
+mv toolchain ../
+rm -rf prebuilts
+
+# Begin vanilla
+tar xf ../kernel.tar.gz
+mv ../toolchain .
+cat arch/arm64/configs/vendor/xiaomi/bengal_defconfig | grep -v "CONFIG_KSU=y" > arch/arm64/configs/vendor/xiaomi/bengal_defconfig.1
+mv arch/arm64/configs/vendor/xiaomi/bengal_defconfig.1 arch/arm64/configs/vendor/xiaomi/bengal_defconfig
+
+bash KernelSU-Next/kernel/setup.sh --cleanup
+bash KernelSU/kernel/setup.sh --cleanup
+
+cat hani-ci.sh | grep -v KBUILD_BUILD_USER > hani-ci.sh.1
+mv hani-ci.sh.1 hani-ci.sh
+cat hani-ci.sh | grep -v KBUILD_BUILD_HOST > hani-ci.sh.1
+mv hani-ci.sh.1 hani-ci.sh
+export KBUILD_BUILD_USER=user
+export KBUILD_BUILD_HOST=localhost
+
+# Build it
+bash hani-ci.sh --build || exit 0
+
+# Cleanup
+mv 4.19*.zip ../
+wget https://github.com/Joe7500/Builds/blob/main/crave/gofile.sh || exit 0
+mv toolchain ../
+rm -rf *
+
+
+# Begin KSU
+
