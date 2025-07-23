ff9793a4c2848dab82ad5188d90807080d350802
Joe7500 <62238620+Joe7500@users.noreply.github.com>
2025-07-23 23:24:24 +0200

    Update resync-harder.sh

--- remote/utils/resync-harder.sh
+++ remote/utils/resync-harder.sh
@@ -35,6 +35,17 @@ main() {
             rm -rf "$repo_path/$repo_name"
             rm -rf ".repo/project/$repo_path/$repo_name"/*.git
         done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
+        while IFS= read -r line; do
+            # Extract repository name and path from the error message
+            repo_info=$(echo "$line" | awk -F': ' '{print $NF}')
+            repo_path=$(dirname "$repo_info")
+            repo_name=$(basename "$repo_info")
+            # Save the deletion path to a text file
+            echo "Deleted repository: $repo_info" | tee -a deleted_repositories.txt
+            # Delete the repository
+            rm -rf "$repo_path/$repo_name"
+            rm -rf ".repo/project/$repo_path/$repo_name"/*.git
+        done <<< "$(cat /tmp/output.txt | awk '/Failing repos \(checkout\):/ {flag=1; next} /Try/ {flag=0} flag')"
     fi
 
     # Check if there are any failing repositories due to uncommitted changes
