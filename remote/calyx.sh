68bb90624d6d67e4b7766565b6a9253e9a0fb576
Joe7500 <62238620+Joe7500@users.noreply.github.com>
2025-08-21 12:46:40 +0200

    Update calyx.sh

--- remote/calyx.sh
+++ remote/calyx.sh
@@ -106,7 +106,7 @@ rm -rf vendor/xiaomi/chime/
 rm -rf device/xiaomi/chime/
 rm -rf hardware/xiaomi/
 rm -rf prebuilts/clang/host/linux-x86/clang-stablekern/
-curl -o kernel.tar.xz -L "https://github.com/Joe7500/Builds/releases/download/Stuff/kernel.tar.xz" ; check_fail
+curl -o kernel.tar.xz -L "https://github.com/Joe7500/Builds/releases/download/Stuff/kernel-prebuilt-hanikrnl-susfs.tar.xz" ; check_fail
 tar xf kernel.tar.xz ; check_fail
 rm -f kernel.tar.xz
 curl -o lineage-22.1.tar.xz -L "https://github.com/Joe7500/Builds/releases/download/Stuff/lineage-22.1.tar.xz" ; check_fail
