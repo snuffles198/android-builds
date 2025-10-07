1a7321794fe914f3359fe1f363f10b214eb63473
Joe7500 <j7545032@gmail.com>
2025-10-07 14:34:31 +0000

    update

--- remote/rising-16.sh
+++ remote/rising-16.sh
@@ -72,17 +72,17 @@ check_fail () {
 if echo "$@" | grep resume; then
    echo "resuming"
 else
-#   rm -rf .repo/manifests*
-#   repo init $REPO_URL  ; check_fail
-#   cleanup_self
-#   /opt/crave/resync.sh
-echo nope
+   rm -rf .repo/manifests*
+   repo init $REPO_URL  ; check_fail
+   cleanup_self
+   /opt/crave/resync.sh
+echo hello
 fi
 
 # Download trees
 rm -rf kernel/xiaomi/chime/ vendor/xiaomi/chime/ device/xiaomi/chime/ hardware/xiaomi/
 rm -rf prebuilts/clang/host/linux-x86/clang-stablekern/
-curl -o kernel.tar.xz -L "https://github.com/Joe7500/Builds/releases/download/Stuff/kernel-prebuilt-perf-valeryn.tar.xz" ; check_fail
+curl -o kernel.tar.xz -L "https://github.com/Joe7500/Builds/releases/download/Stuff/kernel-prebuilt-perf-valeryn-A16.tar.xz" ; check_fail
 tar xf kernel.tar.xz ; check_fail
 rm -f kernel.tar.xz
 curl -o lineage-22.1.tar.xz -L "https://github.com/Joe7500/Builds/releases/download/Stuff/lineage-22.1.tar.xz" ; check_fail
@@ -118,37 +118,7 @@ sed -i -e 's#ifeq ($(call is-version-lower-or-equal,$(TARGET_KERNEL_VERSION),6.1
 sed -i -e 's#ifeq ($(call is-version-greater-or-equal,$(TARGET_KERNEL_VERSION),5.15),true)#ifeq ($(BOARD_USES_QCOM_HARDWARE),true)#g' vendor/lineage/build/tasks/kernel.mk
 sed -i -e 's#GKI_SUFFIX := /$(shell echo android$(PLATFORM_VERSION)-$(TARGET_KERNEL_VERSION))#NOT_NEEDED_DISCARD_567 := true#g' vendor/lineage/build/tasks/kernel.mk
 
-#cd frameworks/base
-#echo 'diff --git a/core/java/android/view/animation/AnimationUtils.java b/core/java/android/view/animation/AnimationUtils.java
-#index 4f12750fa..9ad79bd5f 100644
-#--- a/core/java/android/view/animation/AnimationUtils.java
-#+++ b/core/java/android/view/animation/AnimationUtils.java
-#@@ -240,7 +240,7 @@ public class AnimationUtils {
-#             throws NotFoundException {
-#         int animStyle = Settings.System.getInt(
-#                 context.getContentResolver(),
-#-                "system_animation_style", 0);
-#+                "system_animation_style", 3);
-#         if (animStyle != lastAnimStyle) {
-#             clearCachedAnimations();
-#             lastAnimStyle = animStyle;' > 1.patch
-#patch -f -p 1 < 1.patch
-#cd -
-
-#cd packages/apps/Personalization
-#echo 'diff --git a/res/xml/rising_settings_themes.xml b/res/xml/rising_settings_themes.xml
-#index 73cbffb..7ce0d40 100644
-#--- a/res/xml/rising_settings_themes.xml
-#+++ b/res/xml/rising_settings_themes.xml
-#@@ -143,7 +143,7 @@
-#             android:dialogTitle="@string/system_animation_style_title"
-#             android:entries="@array/system_animation_styles_entries"
-#             android:entryValues="@array/system_animation_styles_values"
-#-            android:defaultValue="0"
-#+            android:defaultValue="3"
-#             android:su' > 1.patch
-#patch -f -p 1 < 1.patch
-#cd -
+for i in `find hardware/qcom/sdm845/ -xtype l` ; do rm $i; done
 
 # Setup device tree
 cd device/xiaomi/chime
@@ -170,18 +140,13 @@ echo 'PRODUCT_BUILD_PROP_OVERRIDES += \
 if echo $@ | grep GAPPS ; then
    echo "RESERVE_SPACE_FOR_GAPPS := false" >> lineage_chime.mk
    echo 'WITH_GMS := true' >> lineage_chime.mk
-   echo 'TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true' >> lineage_chime.mk
    echo 'TARGET_DEFAULT_PIXEL_LAUNCHER := false' >> lineage_chime.mk
-#   echo 'PRODUCT_SYSTEM_PROPERTIES += persist.sys.default_launcher=0' >> lineage_chime.mk
-#   echo 'PRODUCT_SYSTEM_PROPERTIES += persist.sys.quickswitch_pixel_shipped=1' >> lineage_chime.mk
-#   sed -i -e 's/persist.sys.quickswitch_pixel_shipped=0/persist.sys.quickswitch_pixel_shipped=1/g' vendor/rising/config/properties.mk
+   echo 'TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true' >> lineage_chime.mk
 else
 # VANILLA
    cd ../../../vendor/rising ; git reset --hard ; cd -
    echo "RESERVE_SPACE_FOR_GAPPS := true" >> lineage_chime.mk
    echo 'WITH_GMS := false' >> lineage_chime.mk
-#   echo 'TARGET_DEFAULT_PIXEL_LAUNCHER := false' >> lineage_chime.mk
-   echo 'TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true' >> lineage_chime.m
    echo 'PRODUCT_PACKAGES += Gallery2' >> device.mk
 fi
 
@@ -192,21 +157,6 @@ echo 'ro.sf.blurs_are_expensive=1' >> configs/props/system.prop
 
 cd ../../../
 
-cat device/xiaomi/chime/configs/vintf/manifest.xml | grep -v '</manifest>' > device/xiaomi/chime/configs/vintf/manifest.xml.1
-mv device/xiaomi/chime/configs/vintf/manifest.xml.1 device/xiaomi/chime/configs/vintf/manifest.xml
-echo '   <hal format="hidl">
-        <name>vendor.qti.hardware.servicetracker</name>
-        <transport>hwbinder</transport>
-        <version>1.2</version>
-        <interface>
-            <name>IServicetracker</name>
-            <instance>default</instance>
-        </interface>
-        <fqname>@1.2::IServicetracker/default</fqname>
-   </hal>
-</manifest>' >> device/xiaomi/chime/configs/vintf/manifest.xml
-
-#echo 'persist.sys.activity_anim_perf_override=true' >> device/xiaomi/chime/configs/props/system.prop
 echo 'PERF_ANIM_OVERRIDE := true' >> device/xiaomi/chime/device.mk
 
 cat device/xiaomi/chime/BoardConfig.mk | grep -v TARGET_KERNEL_CLANG_VERSION > device/xiaomi/chime/BoardConfig.mk.1
@@ -215,11 +165,20 @@ echo 'TARGET_KERNEL_CLANG_VERSION := stablekern' >> device/xiaomi/chime/BoardCon
 echo 'VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)' >> device/xiaomi/chime/BoardConfig.mk
 echo 'TARGET_DISABLE_EPPE := true' >> device/xiaomi/chime/device.mk
 echo 'TARGET_DISABLE_EPPE := true' >> device/xiaomi/chime/BoardConfig.mk
+#echo 'BOARD_KERNEL_VERSION := 4.19.325-perf' >> device/xiaomi/chime/BoardConfig.mk
+#echo 'BOARD_KERNEL_CONFIG_FILE := kernel/xiaomi/chime/arch/arm64/configs/vendor/chime_defconfig' >> device/xiaomi/chime/BoardConfig.mk
+#echo 'PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false' >> device/xiaomi/chime/lineage_chime.mk
 
 curl -o default_wallpaper.webp -L https://raw.githubusercontent.com/Joe7500/build-scripts/refs/heads/main/remote/utils/default_wallpaper.webp
 cp -f default_wallpaper.webp vendor/rising/overlays/AndroidOverlay/res/drawable-nodpi
 cp -f default_wallpaper.webp vendor/rising/overlays/AndroidOverlay/res/drawable-sw720dp-nodpi
 cp -f default_wallpaper.webp vendor/rising/overlays/AndroidOverlay/res/drawable-sw600dp-nodpi
+
+cd vendor/xiaomi/chime
+curl -o rising.tar.xz -L https://raw.githubusercontent.com/Joe7500/build-scripts/refs/heads/main/remote/src/rising-vendor.tar.xz
+tar xf rising.tar.xz ; check_fail
+rm rising.tar.xz
+cd -
 
 # Get and decrypt signing keys
 curl -o keys.1  -L https://raw.githubusercontent.com/Joe7500/build-scripts/refs/heads/main/remote/keys/BinlFm0d0LoeeibAVCofXsbYTCtcRHpo
@@ -235,8 +194,8 @@ source build/envsetup.sh
 source build/make/envsetup.sh
 source build/envsetup.sh
 source build/make/envsetup.sh
-#source build/envsetup.sh
-#source build/envsetup.sh
+source build/envsetup.sh
+source build/envsetup.sh
 export BUILD_USERNAME=user
 export BUILD_HOSTNAME=localhost
 export KBUILD_BUILD_USER=user
@@ -280,126 +239,6 @@ rm -f genota.sh
 
 TIME_TAKEN=`printf '%dh:%dm:%ds\n' $((SECONDS/3600)) $((SECONDS%3600/60)) $((SECONDS%60))`
 notify_send "Build $PACKAGE_NAME on crave.io completed. $TIME_TAKEN."
-
-
-#cleanup_self
-#exit 0
-
-# setup dev tree for gapps
-cd device/xiaomi/chime
-rm -rf *
-git reset --hard
-git revert --no-edit ea4aba08985fe0addebcaed19a86e86bad64239c #squiggly
-
-cat lineage_chime.mk | grep -v "RESERVE_SPACE_FOR_GAPPS" > lineage_chime.mk.1
-mv lineage_chime.mk.1 lineage_chime.mk
-cat lineage_chime.mk | grep -v "WITH_GMS" > lineage_chime.mk.1
-mv lineage_chime.mk.1 lineage_chime.mk
-
-export RISING_MAINTAINER="Joe"
-echo 'RISING_MAINTAINER="Joe"' >> lineage_chime.mk
-echo 'RISING_MAINTAINER := Joe'  >> lineage_chime.mk
-echo 'PRODUCT_BUILD_PROP_OVERRIDES += \
-    RisingChipset="Chime" \
-    RisingMaintainer="Joe"' >> lineage_chime.mk
-
-# GAPPS
-#if echo GAPPS | grep GAPPS ; then
-   echo "RESERVE_SPACE_FOR_GAPPS := false" >> lineage_chime.mk
-   echo 'WITH_GMS := true' >> lineage_chime.mk
-   echo 'TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true' >> lineage_chime.mk
-   echo 'TARGET_DEFAULT_PIXEL_LAUNCHER := false' >> lineage_chime.mk
-#else
-
-cat lineage_chime.mk | grep -v "TARGET_ENABLE_BLUR" > lineage_chime.mk.1
-mv lineage_chime.mk.1 lineage_chime.mk
-echo 'TARGET_ENABLE_BLUR := true'  >> lineage_chime.mk
-echo 'ro.sf.blurs_are_expensive=1' >> configs/props/system.prop
-
-cd ../../../
-
-cat device/xiaomi/chime/configs/vintf/manifest.xml | grep -v '</manifest>' > device/xiaomi/chime/configs/vintf/manifest.xml.1
-mv device/xiaomi/chime/configs/vintf/manifest.xml.1 device/xiaomi/chime/configs/vintf/manifest.xml
-echo '   <hal format="hidl">
-        <name>vendor.qti.hardware.servicetracker</name>
-        <transport>hwbinder</transport>
-        <version>1.2</version>
-        <interface>
-            <name>IServicetracker</name>
-            <instance>default</instance>
-        </interface>
-        <fqname>@1.2::IServicetracker/default</fqname>
-   </hal>
-</manifest>' >> device/xiaomi/chime/configs/vintf/manifest.xml
-
-#echo 'persist.sys.activity_anim_perf_override=true' >> device/xiaomi/chime/configs/props/system.prop
-echo 'PERF_ANIM_OVERRIDE := true' >> device/xiaomi/chime/device.mk
-
-cat device/xiaomi/chime/BoardConfig.mk | grep -v TARGET_KERNEL_CLANG_VERSION > device/xiaomi/chime/BoardConfig.mk.1
-mv device/xiaomi/chime/BoardConfig.mk.1 device/xiaomi/chime/BoardConfig.mk
-echo 'TARGET_KERNEL_CLANG_VERSION := stablekern' >> device/xiaomi/chime/BoardConfig.mk
-echo 'VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)' >> device/xiaomi/chime/BoardConfig.mk
-echo 'TARGET_DISABLE_EPPE := true' >> device/xiaomi/chime/device.mk
-echo 'TARGET_DISABLE_EPPE := true' >> device/xiaomi/chime/BoardConfig.mk
-
-curl -o default_wallpaper.webp -L https://raw.githubusercontent.com/Joe7500/build-scripts/refs/heads/main/remote/utils/default_wallpaper.webp
-cp -f default_wallpaper.webp vendor/rising/overlays/AndroidOverlay/res/drawable-nodpi
-cp -f default_wallpaper.webp vendor/rising/overlays/AndroidOverlay/res/drawable-sw720dp-nodpi
-cp -f default_wallpaper.webp vendor/rising/overlays/AndroidOverlay/res/drawable-sw600dp-nodpi
-
-# Build it
-set +v
-
-source build/envsetup.sh
-source build/make/envsetup.sh
-source build/envsetup.sh
-source build/make/envsetup.sh
-#source build/envsetup.sh
-#source build/envsetup.sh
-export BUILD_USERNAME=user
-export BUILD_HOSTNAME=localhost
-export KBUILD_BUILD_USER=user
-export KBUILD_BUILD_HOST=localhost
-riseup chime user                 ; check_fail
-mka installclean
-rise b                           ; check_fail
-
-set -v
-
-echo success > result.txt
-notify_send "Build $PACKAGE_NAME on crave.io succeeded."
-
-# Upload output to pixeldrain
-cp out/target/product/chime/$PACKAGE_NAME*.zip .
-GO_FILE=`ls --color=never -1tr $PACKAGE_NAME*.zip | tail -1`
-GO_FILE_MD5=`md5sum "$GO_FILE"`
-GO_FILE=`pwd`/$GO_FILE
-if [[ ! -f $GO_FILE ]]; then
-   GO_FILE=builder.sh
-fi
-curl -T "$GO_FILE" -u :$PDAPIKEY https://pixeldrain.com/api/file/ > out.json
-PD_ID=`cat out.json | cut -d '"' -f 4`
-notify_send "MD5:$GO_FILE_MD5 https://pixeldrain.com/u/$PD_ID"
-rm -f out.json
-
-# Upload file to SF
-curl -o keys.1  -L https://raw.githubusercontent.com/Joe7500/build-scripts/refs/heads/main/remote/keys/usfJoFvObArLx0KmBzwerPPTzliixTN2
-gpg --pinentry-mode=loopback --passphrase "$GPG_PASS_1" -d keys.1 > keys.2
-gpg --pinentry-mode=loopback --passphrase "$GPG_PASS_2" -d keys.2 > sf
-chmod a-x sf
-chmod go-rwx sf
-rsync -avP -e 'ssh -i ./sf -o "StrictHostKeyChecking accept-new"' $GO_FILE $SF_URL
-rm -f keys.1 keys.2 sf
-
-# Generate and send OTA json file
-curl -o genota.sh -L https://raw.githubusercontent.com/Joe7500/Builds/refs/heads/main/genota.sh
-bash genota.sh "$GENOTA_ARG_1" "$GENOTA_ARG_2" "$GO_FILE"
-curl -L -F document=@"$GO_FILE.json.txt" -F caption="OTA $GO_FILE.json.txt" -F chat_id="$TG_CID" -X POST https://api.telegram.org/bot$TG_TOKEN/sendDocument > /dev/null 2>&1
-rm -f genota.sh
-
-TIME_TAKEN=`printf '%dh:%dm:%ds\n' $((SECONDS/3600)) $((SECONDS%3600/60)) $((SECONDS%60))`
-notify_send "Build $PACKAGE_NAME on crave.io completed. $TIME_TAKEN."
-
 
 cleanup_self
 exit 0
