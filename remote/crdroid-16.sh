17eb182e09884b7c836bfb7963b34800f067efb8
Joe7500 <62238620+Joe7500@users.noreply.github.com>
2025-12-22 18:35:09 +0200

    Improve patching process with error checking
    Added error checking after patching to ensure success.

--- remote/crdroid-16.sh
+++ remote/crdroid-16.sh
@@ -119,8 +119,8 @@ if [ $? -ne 0 ] ; then
    cd frameworks/base/
    curl -o 1.patch -L https://github.com/AxionAOSP/android_frameworks_base/commit/f89e8fa592233d86ad2cabf81df245c4003587cb.patch
    curl -o 2.patch -L https://github.com/AxionAOSP/android_frameworks_base/commit/6909a748157404e9150586b9c0860fdb81dd54cc.patch
-   patch -p 1 -f < 1.patch
-   patch -p 1 -f < 2.patch
+   patch -p 1 -f < 1.patch ; check_fail
+   patch -p 1 -f < 2.patch ; check_fail
    cd ../../
 fi
 
