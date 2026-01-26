b7135222cc8b7973d14d188ebd32aa220241db57
Joe7500 <62238620+Joe7500@users.noreply.github.com>
2026-01-26 18:02:34 +0200

    Update pixelos.sh

--- remote/pixelos.sh
+++ remote/pixelos.sh
@@ -117,13 +117,13 @@ sed -i -e 's#ifeq ($(call is-version-lower-or-equal,$(TARGET_KERNEL_VERSION),6.1
 sed -i -e 's#ifeq ($(call is-version-greater-or-equal,$(TARGET_KERNEL_VERSION),5.15),true)#ifeq ($(BOARD_USES_QCOM_HARDWARE),true)#g' vendor/lineage/build/tasks/kernel.mk
 sed -i -e 's#GKI_SUFFIX := /$(shell echo android$(PLATFORM_VERSION)-$(TARGET_KERNEL_VERSION))#NOT_NEEDED_DISCARD_567 := true#g' vendor/lineage/build/tasks/kernel.mk
 
-grep activity_anim_perf_override frameworks/base/core/java/android/view/animation/AnimationUtils.java
-if [ $? -ne 0 ] ; then
-   cd frameworks/base/
-   curl -o 1.patch -L https://raw.githubusercontent.com/Joe7500/build-scripts/refs/heads/main/remote/src/AnimationUtils.java.patch
-   patch -p 1 -f < 1.patch ; check_fail
-   cd ../../
-fi
+#grep activity_anim_perf_override frameworks/base/core/java/android/view/animation/AnimationUtils.java
+#if [ $? -ne 0 ] ; then
+#   cd frameworks/base/
+#   curl -o 1.patch -L https://raw.githubusercontent.com/Joe7500/build-scripts/refs/heads/main/remote/src/AnimationUtils.java.patch
+#   patch -p 1 -f < 1.patch ; check_fail
+#   cd ../../
+#fi
 
 rm -f hardware/xiaomi/megvii/Android.bp
 
@@ -165,6 +165,8 @@ echo 'PERF_ANIM_OVERRIDE := true' >> device/xiaomi/chime/BoardConfig.mk
 
 echo 'TARGET_DISABLE_EPPE := true' >> device/xiaomi/chime/device.mk
 echo 'TARGET_DISABLE_EPPE := true' >> device/xiaomi/chime/BoardConfig.mk
+
+echo 'PRODUCT_ENABLE_UFFD_GC := true' >> device/xiaomi/chime/device.mk
 
 cp device/xiaomi/chime/lineage_chime.mk device/xiaomi/chime/custom_chime.mk
 
