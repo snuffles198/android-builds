d6ee45214e3c4c57fc42c589080d6858727a9e70
Joe7500 <j7545032@gmail.com>
2026-07-12 15:06:50 +0000

    update

---
+++ remote/lineage-24-beta.sh
@@ -0,0 +1,285 @@
+#!/bin/bash
+
+source ~/android-builds/dev-secrets/telegram.sh
+source ~/android-builds/dev-secrets/secrets.sh
+source ~/android-builds/dev-secrets/ntfy.sh
+source /home/admin/.profile
+source /home/admin/.bashrc
+source /tmp/crave_bashrc
+
+mkdir -p /tmp/src
+if [ ! -d /tmp/src/android ] || [ -L /tmp/src/android ]; then
+   rm -rf /tmp/src/android
+   ln -s "$PWD" /tmp/src/android
+fi
+
+cd /tmp/src/android/
+
+set -v
+
+# Template helper variables
+PACKAGE_NAME=lineage-24
+VARIANT_NAME=user
+BUILD_TYPE=vanilla
+DEVICE_BRANCH=lineage-24.0-BETA
+VENDOR_BRANCH=lineage-24.0-BETA
+XIAOMI_BRANCH=lineage-23.2
+GENOTA_ARG_1="lineage"
+GENOTA_ARG_2="24"
+REPO_PARAMS=" --git-lfs --depth=1 --no-tags --no-clone-bundle"
+REPO_URL="-u https://github.com/LineageOS/android.git -b lineage-24.0 $REPO_PARAMS"
+OTA_SED_STRING="https://download.lineageos.org/api/v1/{device}/{type}/{incr}"
+OTA_SED_REPLACE_STRING="https://raw.githubusercontent.com/Joe7500/Builds/main/$PACKAGE_NAME.$VARIANT_NAME.$BUILD_TYPE.chime.json"
+SECONDS=0
+export TZ=Africa/Harare
+if echo $@ | grep "JJ_SPEC:" ; then export JJ_SPEC=`echo $@ | cut -d ":" -f 2` ; fi
+TG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+
+# Send push notifications
+notify_send() {
+   local MSG
+   MSG="$@"
+   curl -s -X POST $TG_URL -d chat_id=$TG_CID -d text="$MSG - $BUILD_TYPE `date`. JJ_SPEC:$JJ_SPEC" > /dev/null 2>&1
+   curl -s -d "$MSG - $BUILD_TYPE `date`. JJ_SPEC:$JJ_SPEC" "ntfy.sh/$NTFYSUB" > /dev/null 2>&1
+}
+
+notify_send "Build $PACKAGE_NAME on crave.io started."
+
+# Always cleanup
+cleanup_self () {
+   cd /tmp/src/android/
+   rm -rf keys.1 keys.2 keys.tar tdl.1 tdl.2 tdl.tar tdl.zip sf
+   rm -rf vendor/lineage-priv/keys vendor/lineage-priv
+   rm -rf priv-keys .config/b2/ /home/admin/.config/b2/
+   rm -rf device/xiaomi/chime/ vendor/xiaomi/chime/ kernel/xiaomi/chime/ hardware/xiaomi/
+   rm -rf prebuilts/clang/kernel/linux-x86/clang-stablekern/ prebuilts/clang/host/linux-x86/clang-stablekern/
+   cd packages/apps/Updater/ && git reset --hard && cd -
+   cd packages/modules/Connectivity/ && git reset --hard && cd -
+   rm -f InterfaceController.java.patch wfdservice.rc.patch strings.xml* builder.sh goupload.sh GOFILE.txt
+   rm -rf /tmp/android-certs* /home/admin/venv/ custom_scripts/
+   cd /home/admin
+   rm -rf .tdl LICENSE  README.md  README_zh.md  tdl  tdl_key  tdl_Linux_64bit.tar.gz* venv tdl.zip tdl_Linux.tgz tdl.sh
+   cd /tmp/src/android/
+}
+
+# Better than ' || exit 1 '
+check_fail () {
+   if [ $? -ne 0 ]; then 
+       if ls out/target/product/chime/$PACKAGE_NAME*.zip; then
+          notify_send "Build $PACKAGE_NAME on crave.io softfailed."
+          echo weird. build failed but OTA package exists.
+          cleanup_self
+          echo softfail > result.txt
+          exit 1
+       else
+          notify_send "Build $PACKAGE_NAME on crave.io failed."
+          echo "oh no. script failed"
+          curl -L -F document=@"out/error.log" -F caption="error log" -F chat_id="$TG_CID" -X POST https://api.telegram.org/bot$TG_TOKEN/sendDocument > /dev/null 2>&1
+          cleanup_self
+          echo fail > result.txt
+          exit 1 
+       fi
+   fi
+}
+
+# repo sync. or not.
+if echo "$@" | grep resume; then
+   echo "resuming"
+else
+   rm -rf .repo/manifests*
+   repo init $REPO_URL --git-lfs ; check_fail
+   cleanup_self
+   /opt/crave/resync.sh ; check_fail
+echo hi
+fi
+
+# Setup lineage-24-BETA source
+export GIT_CONFIG_PARAMETERS="'pull.rebase=true'"
+
+rm -rf .repo/manifests
+curl -o manifest-l24.tar.xz -L https://github.com/snuffles198/android-builds/raw/refs/heads/main/remote/src/manifest-l24.tar.xz ; check_fail
+tar xvf manifest-l24.tar.xz ; check_fail
+
+repo forall -c "git rebase --abort ; git cherry-pick --abort  ; git reset --hard HEAD ; git clean -fdx; git branch -D auto"
+repo sync --nmu -c --force-sync --no-clone-bundle --optimized-fetch --no-tags --prune -d --force-checkout --force-remove-dirty
+
+cd vendor/qcom/opensource/vibrator/
+git switch lineage-24.0
+cd -
+
+./lineage/scripts/repopick/repopick.py -r -p -i -t "CP2A" 2>&1 | tee CP2A.log
+./lineage/scripts/repopick/repopick.py -j 8 -r -p -i -Q "branch:lineage-24.0" 2>&1 | tee lineage-24.0.log
+
+cd device/lineage/sepolicy/
+wget https://github.com/LineageOS/android_device_lineage_sepolicy/commit/29ebee04ead17fb9c647e4e1219c83d8760574e7.patch
+patch -p 1 -f < 29ebee04ead17fb9c647e4e1219c83d8760574e7.patch
+cd -
+
+for i in hardware/qcom-caf/sm8450-6.6/ hardware/qcom-caf/sm8750/ hardware/qcom-caf/sm8550 hardware/qcom-caf/sm8450 hardware/qcom-caf/sm8650 ; do
+  for x in `find $i | grep Android.bp` ; do
+    rm -fv $x
+  done
+done
+
+cd vendor/qcom/opensource/interfaces
+git rebase --abort ; git cherry-pick --abort
+cd -
+
+cd external/aac
+ln -s cpp/libAACdec/ .
+ln -s cpp/libAACenc/ .
+ln -s cpp/libSYS/ .
+cd -
+
+cd hardware/qcom-caf/wlan
+echo 'diff --git a/qcwcn/wifi_hal/nan_pairing.cpp b/qcwcn/wifi_hal/nan_pairing.cpp
+index c3f379a..ed6cfc1 100644
+--- a/qcwcn/wifi_hal/nan_pairing.cpp
++++ b/qcwcn/wifi_hal/nan_pairing.cpp
+@@ -2649,9 +2649,14 @@ void nan_pairing_set_password(struct nan_pairing_peer_info *peer, u8 *passphrase
+     }
+     strlcpy(peer->passphrase, reinterpret_cast<const char *> (passphrase),
+             len + 1);
+-    pt = sae_derive_pt(NULL, pairing_ssid, pairing_ssid_len,
+-                       (const u8 *)passphrase, len,
+-                       peer->sae_password_id);
++    size_t pp_id_len = 0;
++    if (peer->sae_password_id)
++        pp_id_len = strlen(peer->sae_password_id);
++    pt = sae_derive_pt(NULL,
++                       pairing_ssid, pairing_ssid_len,
++                      (const u8 *)passphrase, len,
++                      (const u8 *)peer->sae_password_id,
++                      pp_id_len);
+     pasn_set_pt(peer->pasn, pt);
+     /* Set passpharse for Pairing Responder to validate PASN auth1 frame*/
+     pasn_set_password(peer->pasn, peer->passphrase);' > 1.patch
+patch -p 1 -f < 1.patch
+cd -
+
+TIME_TAKEN=`printf '%dh:%dm:%ds\n' $((SECONDS/3600)) $((SECONDS%3600/60)) $((SECONDS%60))`
+notify_send "Build $PACKAGE_NAME on crave.io repo sync done. $TIME_TAKEN."
+
+# Download trees
+rm -rf kernel/xiaomi/chime/ vendor/xiaomi/chime/ device/xiaomi/chime/ hardware/xiaomi/
+rm -rf prebuilts/clang/host/linux-x86/clang-stablekern/
+curl -o kernel.tar.xz -L "https://github.com/Joe7500/Builds/releases/download/Stuff/kernel-prebuilt-perf-valeryn-A17.tar.xz" ; check_fail
+tar xf kernel.tar.xz ; check_fail ; rm -f kernel.tar.xz
+curl -o lineage-22.1.tar.xz -L "https://github.com/Joe7500/Builds/releases/download/Stuff/lineage-22.1.tar.xz" ; check_fail
+tar xf lineage-22.1.tar.xz ; check_fail ; rm -f lineage-22.1.tar.xz
+curl -o toolchain.tar.xz -L "https://github.com/Joe7500/Builds/releases/download/Stuff/toolchain.tar.xz" ; check_fail
+tar xf toolchain.tar.xz ; check_fail ; rm -f toolchain.tar.xz
+git clone https://github.com/snuffles198/device_tree -b $DEVICE_BRANCH device/xiaomi/chime ; check_fail
+git clone https://github.com/snuffles198/vendor_tree -b $VENDOR_BRANCH vendor/xiaomi/chime ; check_fail
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b $XIAOMI_BRANCH hardware/xiaomi ; check_fail
+
+# Setup AOSP source 
+patch -f -p 1 < wfdservice.rc.patch ; check_fail
+cd packages/modules/Connectivity/ && git reset --hard && cd -
+patch -f -p 1 < InterfaceController.java.patch ; check_fail
+rm -f InterfaceController.java.patch wfdservice.rc.patch strings.xml.*
+
+cd packages/apps/Updater/ && git reset --hard && cd -
+cp packages/apps/Updater/app/src/main/res/values/strings.xml strings.xml
+cat strings.xml | sed -e "s#$OTA_SED_STRING#$OTA_SED_REPLACE_STRING#g" > strings.xml.1
+cp strings.xml.1 packages/apps/Updater/app/src/main/res/values/strings.xml
+rm -f strings.xml.1
+check_fail
+
+sed -i -e 's#ifeq ($(call is-version-lower-or-equal,$(TARGET_KERNEL_VERSION),6.1),true)#ifeq ($(BOARD_USES_QCOM_HARDWARE),true)#g' vendor/lineage/build/tasks/kernel.mk
+sed -i -e 's#ifeq ($(call is-version-greater-or-equal,$(TARGET_KERNEL_VERSION),5.15),true)#ifeq ($(BOARD_USES_QCOM_HARDWARE),true)#g' vendor/lineage/build/tasks/kernel.mk
+sed -i -e 's#GKI_SUFFIX := /$(shell echo android$(PLATFORM_VERSION)-$(TARGET_KERNEL_VERSION))#NOT_NEEDED_DISCARD_567 := true#g' vendor/lineage/build/tasks/kernel.mk
+
+#grep activity_anim_perf_override frameworks/base/core/java/android/view/animation/AnimationUtils.java
+#if [ $? -ne 0 ] ; then
+#   cd frameworks/base/
+#  curl -o 1.patch -L https://raw.githubusercontent.com/snuffles198/android-builds/refs/heads/main/remote/src/AnimUtils-A16-QPR2.java.patch
+#  patch -p 1 -f < 1.patch ; check_fail
+#  cd ../../
+#fi
+
+# Setup device tree
+cd device/xiaomi/chime
+git revert --no-edit ea4aba08985fe0addebcaed19a86e86bad64239c # Squiggly media progress bar, depends on ROM
+git revert --no-edit 0a790d4fabf2745212e827d5868f9703b2ec47ed #blur by defaut
+curl -o configs/powerhint.json -L "https://raw.githubusercontent.com/snuffles198/android-builds/refs/heads/main/remote/src/powerhint.json.axion.7.txt" ; check_fail
+cat BoardConfig.mk | grep -v TARGET_KERNEL_CLANG_VERSION > BoardConfig.mk.1
+mv BoardConfig.mk.1 BoardConfig.mk
+echo 'TARGET_KERNEL_CLANG_VERSION := stablekern' >> BoardConfig.mk
+echo 'VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)' >> BoardConfig.mk
+echo 'ro.launcher.blur.appLaunch=0' >> configs/props/product.prop
+# PRODUCT_SYSTEM_PROPERTIES += ro.surface_flinger.supports_background_blur=1
+echo 'ro.surface_flinger.supports_background_blur=1' >> configs/props/system.prop
+echo 'persist.sys.sf.disable_blurs=1' >> configs/props/product.prop
+echo 'ro.sf.blurs_are_expensive=1' >> configs/props/product.prop
+echo 'TARGET_ENABLE_BLUR := true' >> lineage_chime.mk 
+cd -
+
+echo 'persist.sys.activity_anim_perf_override=true' >> device/xiaomi/chime/configs/props/product.prop
+echo 'PERF_ANIM_OVERRIDE := true' >> device/xiaomi/chime/device.mk
+echo 'PERF_ANIM_OVERRIDE := true' >> device/xiaomi/chime/BoardConfig.mk
+
+#curl -o audio_effects.xml -L https://raw.githubusercontent.com/snuffles198/android-builds/refs/heads/main/remote/src/audio_effects_viper.xml
+#mv audio_effects.xml device/xiaomi/chime/audio/audio_effects.xml
+#echo '$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)' >> device/xiaomi/chime/device.mk
+#if ! ls packages/apps/ViPER4AndroidFX/config.mk ; then
+#   git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+#   check_fail
+#fi
+
+#echo 'TARGET_DISABLE_EPPE := true' >> device/xiaomi/chime/device.mk
+#echo 'TARGET_DISABLE_EPPE := true' >> device/xiaomi/chime/BoardConfig.mk
+
+# Setup kernel
+
+# Get and decrypt signing keys
+curl -o keys.1  -L https://raw.githubusercontent.com/snuffles198/android-builds/refs/heads/main/remote/keys/BinlFm0d0LoeeibAVCofXsbYTCtcRHpo
+gpg --pinentry-mode=loopback --passphrase "$GPG_PASS_1" -d keys.1 > keys.2
+gpg --pinentry-mode=loopback --passphrase "$GPG_PASS_2" -d keys.2 > keys.tar
+tar xf keys.tar
+rm -f keys.1 keys.2 keys.tar
+
+# Build it
+
+set +v
+
+source build/envsetup.sh          ; check_fail
+source build/envsetup.sh
+source build/envsetup.sh
+export BUILD_USERNAME=user
+export BUILD_HOSTNAME=localhost
+export KBUILD_BUILD_USER=user
+export KBUILD_BUILD_HOST=localhost
+lunch lineage_chime-cp2a-user              ; check_fail
+mka installclean
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS=false WITH_DEXPREOPT=false mka bacon -j$(nproc --all)        ; check_fail
+
+set -v
+
+echo success > result.txt
+notify_send "Build $PACKAGE_NAME on crave.io succeeded."
+
+# Upload output to pixeldrain
+cp out/target/product/chime/$PACKAGE_NAME*.zip .
+GO_FILE=`ls --color=never -1tr $PACKAGE_NAME*.zip | tail -1`
+GO_FILE_MD5=`md5sum "$GO_FILE"`
+GO_FILE=`pwd`/$GO_FILE
+if [[ ! -f $GO_FILE ]]; then
+   GO_FILE=builder.sh
+fi
+curl -T "$GO_FILE" -u :$PDAPIKEY https://pixeldrain.com/api/file/ > out.json
+PD_ID=`cat out.json | cut -d '"' -f 4`
+notify_send "MD5:$GO_FILE_MD5 https://pixeldrain.com/u/$PD_ID"
+rm -f out.json
+
+# Generate and send OTA json file
+curl -o genota.sh -L https://raw.githubusercontent.com/Joe7500/Builds/refs/heads/main/genota.sh
+bash genota.sh "$GENOTA_ARG_1" "$GENOTA_ARG_2" "$GO_FILE"
+curl -L -F document=@"$GO_FILE.json.txt" -F caption="OTA $GO_FILE.json.txt" -F chat_id="$TG_CID" -X POST https://api.telegram.org/bot$TG_TOKEN/sendDocument > /dev/null 2>&1
+rm -f genota.sh
+
+TIME_TAKEN=`printf '%dh:%dm:%ds\n' $((SECONDS/3600)) $((SECONDS%3600/60)) $((SECONDS%60))`
+notify_send "Build $PACKAGE_NAME on crave.io completed. $TIME_TAKEN."
+
+cleanup_self
+exit 0
